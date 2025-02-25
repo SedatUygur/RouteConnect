f100dccdfdba0657a41bc8ab2fa101882005169f
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-25 21:56:37 +0300

    🎉 feat : migrate the database, create Trip model

--- backend/trip/models.py
+++ backend/trip/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Trip(models.Model):
+    """
+    Stores the basic trip info.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    current_location = models.CharField(max_length=255)
+    pickup_location = models.CharField(max_length=255)
+    dropoff_location = models.CharField(max_length=255)
+    current_cycle_hours_used = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    total_distance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    estimated_duration = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Trip {self.id} from {self.pickup_location} to {self.dropoff_location}"
