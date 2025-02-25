79f0c9691607ae38b832091ad6784c1e205a8b80
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-26 00:42:16 +0300

    🎉 feat : add trips endpoints to the project urls

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from trip.views import TripViewSet
+
+router = DefaultRouter()
+router.register(r'trips', TripViewSet, basename='trip')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
