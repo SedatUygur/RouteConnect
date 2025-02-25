7e7a7fa7fff76c3f29cf8457cd3beea256557c6c
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-25 23:45:44 +0300

    🎉 feat : add pickup stop

--- backend/trip/services/route_and_hos_service.py
+++ backend/trip/services/route_and_hos_service.py
@@ -1,4 +1,8 @@
+import datetime
+
+from django.utils import timezone
 from .map_api_client import get_route_data
+from ..models import Stop
 
 def calculate_trip_stops(trip):
     route_info = get_route_data(trip.current_location, trip.dropoff_location)
@@ -13,3 +17,13 @@ def calculate_trip_stops(trip):
     # Clear existing stops/logs for recalculation
     trip.stops.all().delete()
     trip.logs.all().delete()
+    start_dt = timezone.now()
+
+    # 1) Add pickup stop
+    pickup_stop = Stop.objects.create(
+        trip=trip,
+        stop_type="Pickup",
+        location=trip.pickup_location,
+        start_time=start_dt,
+        end_time=start_dt + datetime.timedelta(hours=1)
+    )
