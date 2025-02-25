79aa77debb9f12bb766091e738a355bf360403c8
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-25 21:31:14 +0300

    🎉 feat : configure installed apps and database

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'trip',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +78,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DB_ENGINE = os.getenv("DB_ENGINE", default="")
+DB_NAME = os.getenv("DB_NAME", default="")
+DB_USER = os.getenv("DB_USER", default="")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="")
+DB_HOST = os.getenv("DB_HOST", default="")
+DB_PORT = os.getenv("DB_PORT", default="")
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': DB_ENGINE,
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT
     }
 }
 
