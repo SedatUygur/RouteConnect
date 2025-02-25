9e560f72a937d59743c5b754df2ae0e06ab56d3f
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-25 22:26:13 +0300

    🎉 feat : create calculate route action

--- backend/trip/views.py
+++ backend/trip/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Trip
 from .serializers import TripSerializer
@@ -7,3 +8,10 @@ from .serializers import TripSerializer
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+    @action(detail=True, methods=['post'])
+    def calculate_route(self, request, pk=None):
+        trip = self.get_object()
+        # Calculate trip stops.
+        serializer = TripSerializer(trip)
+        return Response(serializer.data, status=status.HTTP_200_OK)
