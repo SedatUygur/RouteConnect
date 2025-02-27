07ebd73d95ea3dbb16253cdab5c41650e3cdbc83
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-27 20:51:20 +0300

    🎉 feat : create interfaces for DailyLogPdf component

--- frontend/components/DailyLogPdf.tsx
+++ frontend/components/DailyLogPdf.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import React from 'react';
+
+interface DailyLog {
+    id: number;
+    date: string;
+    total_driving: number;
+    total_on_duty: number;
+    total_off_duty: number;
+    total_sleeper_berth: number;
+}
+
+interface DailyLogPdfProps {
+    logs: DailyLog[];
+}
+
 export default function DailyLogPdf() {
   return (
     <div>
